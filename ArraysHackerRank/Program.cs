0eac67bca019a11934fc18e6ec0b8d84f231b1b5
Özgür KILIÇ <ozgurkilic147@gmail.com>
2025-05-20 04:59:49 +0200

    Initial commit

---
+++ ArraysHackerRank/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ArraysHackerRank;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*List<int> numbers = new List<int>(){1,2,3,4,5};
+        List<int> rotatedNumbers = rotateLeft(2, numbers);
+        foreach (var variable in rotatedNumbers)
+        Console.WriteLine(variable);*/
+        
+        
+        /*List<string> stringList = new List<string>(){"ab", "ab", "abc"};
+        List<string> queries = new List<string>(){"ab", "abc", "abcd", "abcde"};
+        List<int> matchCount = matchingStrings(stringList, queries);
+        foreach (int val in matchCount)
+            Console.WriteLine(val);*/
+    }
+
+    static int TwoDArrayDS(List<List<int>> arr) //https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true
+    {
+        int sum = -63;
+        for (int i = 1; i < 5; i++)
+        {
+            for (int j = 1; j < 5; j++)
+            {
+                if (sum < arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] +
+                    arr[i + 1][j] + arr[i+1][j + 1])
+                    sum = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] 
+                          + arr[i][j] 
+                          + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i+1][j + 1];
+            }
+        }
+
+        return sum;
+    }
+    
+    static List<int> rotateLeft(int d, List<int> arr) //https://www.hackerrank.com/challenges/array-left-rotation/problem?isFullScreen=true
+    {
+        int temp = d % arr.Count;
+        List<int> result = new List<int>();
+        for (int i = temp; i < arr.Count; i++)
+        {
+            result.Add(arr[i]);
+        }
+        for (int i = 0; i < temp; i++)
+        {
+            result.Add(arr[i]);
+        }
+
+        return result;
+    }
+    
+    public static List<int> matchingStrings(List<string> stringList, List<string> queries) //https://www.hackerrank.com/challenges/sparse-arrays/problem?isFullScreen=true
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < queries.Count; i++)
+        {
+            result.Add(0);
+            for (int j = 0; j < stringList.Count; j++)
+            {
+                if (stringList[j].Equals(queries[i]))
+                    result[i]++;
+            }
+        }
+        return result;
+    }
+}
