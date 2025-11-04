8a221d05af3d63ff1203ff7d31403f438886661a
Özgür Kılıç <ozgurkilic147@gmail.com>
2025-11-04 16:07:11 +0100

    The question 3Sum's solution was relocated under the title two pointers-medium questions as intended

--- ArraysHackerRank/Program.cs
+++ ArraysHackerRank/Program.cs
@@ -518,37 +518,6 @@ class Program
         }
         return highest;
     }
-    
-    
-    public IList<IList<int>> ThreeSum(int[] nums) { //https://leetcode.com/problems/3sum
-        //Time Complexity: O(n^2)
-        //Space Complexity: O(m) space for the output list.
-        Array.Sort(nums);
-        List<IList<int>> res = new List<IList<int>>();
-
-        for (int i = 0; i < nums.Length; i++) {
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int l = i + 1, r = nums.Length - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum > 0) {
-                    r--;
-                } else if (sum < 0) {
-                    l++;
-                } else {
-                    res.Add(new List<int> {nums[i], nums[l], nums[r]});
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
-                    }
-                }
-            }
-        }
-        return res;
-    }
     #endregion
     
     #region Two Pointers - NeetCode
@@ -676,6 +645,37 @@ class Program
          */
     }
     
+    
+    //------ Medium Questions ------
+    public IList<IList<int>> ThreeSum(int[] nums) { //https://leetcode.com/problems/3sum
+        //Time Complexity: O(n^2)
+        //Space Complexity: O(m) space for the output list.
+        Array.Sort(nums);
+        List<IList<int>> res = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1, r = nums.Length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.Add(new List<int> {nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
     #endregion
 
     #region Sliding Window - NeetCode
