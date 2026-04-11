bbd60e9a75ea75c6b6733faae616fb5f694360ea
Özgür Kılıç <ozgurkilic147@gmail.com>
2026-04-11 14:28:31 +0200

    ignore build files

--- ArraysHackerRank/obj/Debug/net8.0/ArraysHackerRank.AssemblyInfo.cs
+++ ArraysHackerRank/obj/Debug/net8.0/ArraysHackerRank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArraysHackerRank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f5e344825c4396b27e29e26138f3ee76e1e73f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133b6fb2f5f77471f2478ddddca3650726e9020c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArraysHackerRank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArraysHackerRank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
